60a95c03ec381026a8e5756f85a70ce872077ff8
Sahil Khan <sahilkhan797491@gmail.com>
2022-05-29 02:46:42 +0530

    make it better

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.use(cors())
 app.use("/", contactRoute)
 
 
+//creating a static folder to host
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"))
+    app.get("*", (req,res) =>(
+        res.sendFile(path.resolve(__dirname,"client", "build", "index.html" ))
+    ))
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, console.log('server listening to port 5000 only'));
